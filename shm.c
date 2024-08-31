859f26120561123f0c0e169a88329db43218f97a
Yongyan Liang <ylian121@xe-09.cs.ucr.edu>
2024-08-31 04:25:24 +0000

    shm close

--- shm.c
+++ shm.c
@@ -41,9 +41,36 @@ return 0; //added to remove compiler warning -- you should decide what to return
 
 int shm_close(int id) {
 //you write this too!
+  int matchingID = 0;
+  int i = 0; 
+  struct proc* currentProcess = myproc();
+  acquire(&(shm_table.lock)); 
+  for(i = 0; i < 64; i++){ 
+    if(shm_table.shm_pages[i].id == id){ 
+      shm_table.shm_pages[i].refcnt--;
+      matchingID = 1; 
+      if(shm_table.shm_pages[i].refcnt > 0){
+        break;
+      }
+      else{
+        uint va;
+        for (va = 0; va < currentProcess->sz; va += PGSIZE) {
+          pte_t *pte = walkpgdir(currentProcess->pgdir, (void*)va, 0); 
+          lcr3(V2P(currentProcess->pgdir));
+          if (pte && (*pte & PTE_P) && PTE_ADDR(*pte) == V2P(shm_table.shm_pages[i].frame)) {
+            *pte = 0;
+            lcr3(V2P(currentProcess->pgdir));
+            break;
+          }
+        }
+        shm_table.shm_pages[i].frame = 0;
+        shm_table.shm_pages[i].id = 0;
 
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+        break;
+      }
+    }
+  }
+  release(&(shm_table.lock));
+  return matchingID == 0 ? 1 : 0;
 }
+
